d515e5f6e7611282349d7b55d8fa88b116355ab8
deepakr6242 <deepakr6242@gmail.com>
2018-10-11 17:44:07 +0000

    Adding Comments

--- omnipayapp/views.py
+++ omnipayapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
+# Adding Comments
+
 def hello(request):
    text = """<h2>Welcome to Omnipayapp!</h2>"""
    return HttpResponse(text)
