fb8a500b1a4ea1a96d7540209260103c22657d13
deepakr6242 <deepakr6242@gmail.com>
2018-10-11 17:45:22 +0000

    Adding the change

--- omnipayapp/views.py
+++ omnipayapp/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 # Adding Comments
 
 def hello(request):
-   text = """<h2>Welcome to Omnipayapp!</h2>"""
+   text = """<h2>Welcome to Omnipayapp! and  Change  made</h2>"""
    return HttpResponse(text)
