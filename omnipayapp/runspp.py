2d0b263ed26fc6dd05fa794e0f89ce72b14a791c
deepakr6242 <deepakr6242@gmail.com>
2018-10-12 05:42:21 +0000

    New deploy check

--- omnipayapp/runspp.py
+++ omnipayapp/runspp.py
@@ -1,16 +1,22 @@
 import os
 import random
+import datetime
 
-port_end_digit= random.randint(1,9)
-print port_end_digit
-
-with open('C:\\Python27\\Scripts\\omnipay\\log.txt','a+') as file:
-	file.write("App  is running in 808"+str(port_end_digit)+'\n')
-os.chdir('C:\\Python27\\Scripts\\omnipay')
+def foo():
 
 
-try:
-	os.system('python manage.py runserver 127.0.0.1:808'+str(port_end_digit))
+	port_end_digit= random.randint(1,9)
+	print port_end_digit
 
-except Exceptio as e:
-	print"Exception is due to ",e
+	with open('C:\\Python27\\Scripts\\omnipay\\log.txt','a+') as file:
+		file.write("App  is running in 808"+str(port_end_digit)+str(datetime.datetime.now())+'\n')
+	print"logged"
+	os.chdir('C:\\Python27\\Scripts\\omnipay')
+
+
+	try:
+		os.system('python manage.py runserver 127.0.0.1:808'+str(port_end_digit))
+
+	except Exceptio as e:
+		print"Exception is due to ",e
+foo()
